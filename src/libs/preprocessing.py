2a0c7234cb5c1a79b978f26915dc482f3e796baf
Jules Chapon <jules.b.chapon@gmail.com>
2024-12-07 16:54:58 +0100

    Init repo

---
+++ src/libs/preprocessing.py
@@ -0,0 +1,13 @@
+"""Functions for preprocessing"""
+
+
+def load_data_from_hf(is_origin: bool, is_train: bool) -> None:
+    return None
+
+
+def load_data_from_local(is_origin: bool, is_train: bool) -> None:
+    return None
+
+
+def load_data(is_origin: bool, is_train: bool, is_local: bool) -> None:
+    return None
