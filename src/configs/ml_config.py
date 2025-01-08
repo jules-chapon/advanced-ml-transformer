d2c38141898fd36d1f3417a847e71583c9d8616e
Jules Chapon <jules.b.chapon@gmail.com>
2025-01-08 21:18:43 +0100

    [UPDATE] : add new id_experiment to try on large dataset

--- src/configs/ml_config.py
+++ src/configs/ml_config.py
@@ -31,6 +31,26 @@ EXPERIMENTS_CONFIGS = {
         names.BATCH_SIZE: 16,
         names.BETAS: (0.9, 0.98),
     },
-    1: {},
+    1: {
+        names.MODEL_TYPE: names.TRANSFORMER,
+        # TRANSLATION
+        names.SRC_LANGUAGE: "en",
+        names.TGT_LANGUAGE: "fr",
+        # ARCHITECTURE
+        names.EMBEDDING_DIMENSION: 64,
+        names.MAX_SEQUENCE_LENGTH: 32,
+        names.NB_LAYERS: 2,
+        names.NB_HEADS: 4,
+        names.HEAD_OUTPUT_DIMENSION: 64,
+        names.HEAD_SIZE: 16,  # = EMBEDDING_DIMENSION / NB_HEADS
+        names.DROPOUT: 0.1,
+        names.FEEDFORWARD_DIMENSION: 256,
+        names.DEVICE: "cuda",
+        # TRAINING
+        names.NB_EPOCHS: 2,
+        names.LEARNING_RATE: 1e-4,
+        names.BATCH_SIZE: 16,
+        names.BETAS: (0.9, 0.98),
+    },
     # Add more experiments as needed
 }
