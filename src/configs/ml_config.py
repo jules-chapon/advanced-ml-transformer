77418ae161cb872944f5816f5af4fdea48b3cc8a
Jules Chapon <jules.b.chapon@gmail.com>
2025-01-15 15:08:25 +0100

    [UPDATE] : launch real experiments

--- src/configs/ml_config.py
+++ src/configs/ml_config.py
@@ -158,6 +158,33 @@ EXPERIMENTS_CONFIGS = {
         names.EPSILON: 1e-9,
         names.LAMBDA_INIT: 0.8,
     },
+    7: {
+        names.MODEL_TYPE: names.TRANSFORMER,
+        # TRANSLATION
+        names.SRC_LANGUAGE: "en",
+        names.TGT_LANGUAGE: "fr",
+        names.TOKENIZATION: names.ADVANCED,
+        # ARCHITECTURE
+        names.EMBEDDING_DIMENSION: 64,
+        names.MAX_LENGTH_SRC: 32,
+        names.MAX_LENGTH_TGT: 32,
+        names.MAX_CONTEXT_TGT: 8,
+        names.NB_LAYERS: 4,
+        names.NB_HEADS: 4,
+        names.HEAD_OUTPUT_DIMENSION: 64,
+        names.HEAD_SIZE: 16,  # = EMBEDDING_DIMENSION / NB_HEADS
+        names.DROPOUT: 0.1,
+        names.FEEDFORWARD_DIMENSION: 256,
+        names.DEVICE: "cuda",
+        # TRAINING
+        names.NB_EPOCHS: 2,
+        names.LEARNING_RATE: 1e-4,
+        names.BATCH_SIZE: 32,
+        names.NUM_WORKERS: 4,
+        names.BETAS: (0.9, 0.98),
+        names.EPSILON: 1e-9,
+        names.LAMBDA_INIT: 0.8,
+    },
     104: {
         names.MODEL_TYPE: names.DIFF_TRANSFORMER,
         # TRANSLATION
@@ -196,6 +223,33 @@ EXPERIMENTS_CONFIGS = {
         names.MAX_LENGTH_TGT: 32,
         names.MAX_CONTEXT_TGT: 8,
         names.NB_LAYERS: 1,
+        names.NB_HEADS: 4,
+        names.HEAD_OUTPUT_DIMENSION: 64,
+        names.HEAD_SIZE: 16,  # = EMBEDDING_DIMENSION / NB_HEADS
+        names.DROPOUT: 0.1,
+        names.FEEDFORWARD_DIMENSION: 256,
+        names.DEVICE: "cuda",
+        # TRAINING
+        names.NB_EPOCHS: 2,
+        names.LEARNING_RATE: 1e-4,
+        names.BATCH_SIZE: 32,
+        names.NUM_WORKERS: 4,
+        names.BETAS: (0.9, 0.98),
+        names.EPSILON: 1e-9,
+        names.LAMBDA_INIT: 0.8,
+    },
+    107: {
+        names.MODEL_TYPE: names.TRANSFORMER,
+        # TRANSLATION
+        names.SRC_LANGUAGE: "en",
+        names.TGT_LANGUAGE: "fr",
+        names.TOKENIZATION: names.ADVANCED,
+        # ARCHITECTURE
+        names.EMBEDDING_DIMENSION: 64,
+        names.MAX_LENGTH_SRC: 32,
+        names.MAX_LENGTH_TGT: 32,
+        names.MAX_CONTEXT_TGT: 8,
+        names.NB_LAYERS: 4,
         names.NB_HEADS: 4,
         names.HEAD_OUTPUT_DIMENSION: 64,
         names.HEAD_SIZE: 16,  # = EMBEDDING_DIMENSION / NB_HEADS
--- src/libs/visualization.py
+++ src/libs/visualization.py
@@ -12,5 +12,6 @@ def plot_losses(train_losses: np.ndarray, valid_losses: np.ndarray) -> None:
     plt.xlabel("Epoch")
     plt.xticks([x for x in range(0, train_losses.shape[0] - 1) if x % 10 == 0])
     plt.ylabel("Loss")
+    plt.ylim((0, 10))
     plt.legend(loc="upper right")
     plt.show()
